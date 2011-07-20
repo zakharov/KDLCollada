eec7fdf9fe7314c1b19428b9b848492e80d82cb1
zakharov <alexey.zakharov@gmail.com>
2011-07-20 14:23:20 +0200

    added a simple example and a test collada model

--- include/KDLCollada.h
+++ include/KDLCollada.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   KDLCollada.h
  * Author: alexey
  *
@@ -8,11 +8,18 @@
 #ifndef KDLCOLLADA_H
 #define	KDLCOLLADA_H
 
+#include <kdl/chain.hpp>
+#include <string>
+
 class KDLCollada {
 public:
     KDLCollada();
     KDLCollada(const KDLCollada& orig);
     virtual ~KDLCollada();
+
+    bool load(const std::string& filename, std::vector<KDL::Chain>& kdlChain);
+    bool save(const std::string& filename, std::vector<KDL::Chain>& kdlChain);
+
 private:
 
 };
--- src/KDLCollada.cpp
+++ src/KDLCollada.cpp
@@ -1,18 +1,45 @@
-/* 
+/*
  * File:   KDLCollada.cpp
  * Author: alexey
- * 
+ *
  * Created on July 20, 2011, 8:19 AM
  */
 
 #include "KDLCollada.h"
+#include "KDLColladaExporter.h"
+#include "KDLColladaImporter.h"
 
-KDLCollada::KDLCollada() {
+#include "COLLADASaxFWLLoader.h"
+#include "GeneratedSaxParserCoutErrorHandler.h"
+#include "COLLADAFWRoot.h"
+
+using namespace std;
+using namespace KDL;
+
+KDLCollada::KDLCollada()
+{
 }
 
-KDLCollada::KDLCollada(const KDLCollada& orig) {
+KDLCollada::KDLCollada(const KDLCollada& orig)
+{
 }
 
-KDLCollada::~KDLCollada() {
+KDLCollada::~KDLCollada()
+{
 }
 
+bool KDLCollada::load(const string& filename, std::vector<Chain>& kdlChain)
+{
+    KDLColladaImporter importer;
+ 	COLLADASaxFWL::Loader loader;
+	COLLADAFW::Root root(&loader, &importer);
+
+
+    return root.loadDocument(filename);
+}
+
+bool KDLCollada::save(const string& filename, std::vector<Chain>& kdlChain)
+{
+    KDLColladaExporter exporter;
+    return false;
+}
