be013afbb23e3263ea1840861310bd16d2a9af07
zakharov <alexey.zakharov@gmail.com>
2011-07-20 09:07:16 +0200

    added cmake template

---
+++ include/KDLCollada.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   KDLCollada.h
+ * Author: alexey
+ *
+ * Created on July 20, 2011, 8:19 AM
+ */
+
+#ifndef KDLCOLLADA_H
+#define	KDLCOLLADA_H
+
+class KDLCollada {
+public:
+    KDLCollada();
+    KDLCollada(const KDLCollada& orig);
+    virtual ~KDLCollada();
+private:
+
+};
+
+#endif	/* KDLCOLLADA_H */
+
---
+++ src/KDLCollada.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   KDLCollada.cpp
+ * Author: alexey
+ * 
+ * Created on July 20, 2011, 8:19 AM
+ */
+
+#include "KDLCollada.h"
+
+KDLCollada::KDLCollada() {
+}
+
+KDLCollada::KDLCollada(const KDLCollada& orig) {
+}
+
+KDLCollada::~KDLCollada() {
+}
+
